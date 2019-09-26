30284273c408cd9f2d29b285e013afeb242864f7
giuliobosco <giuliobva@gmail.com>
2019-09-26 13:25:25 +0200

    fix db.sql - timer

--- sql/db.sql
+++ sql/db.sql
@@ -163,6 +163,7 @@ CREATE TABLE freqline.mic
 
   generator INT NOT NULL ,
   decibel VARCHAR(64) NOT NULL ,
+  timer INT,
   ip VARCHAR(39) NOT NULL ,
   key_ VARCHAR(12) NOT NULL ,
 
@@ -199,6 +200,7 @@ CREATE TABLE freqline.mic_audit
 
   audit_generator INT NOT NULL ,
   audit_decibel VARCHAR(64) NOT NULL ,
+  audit_timer INT,
   audit_ip VARCHAR(39) NOT NULL ,
   audit_key_ VARCHAR(12) NOT NULL ,
 
