be98fb4b65ac58cdc74271a255f22186cb589959
giuliobosco <giuliobva@gmail.com>
2019-11-15 15:41:53 +0100

    add queries.AccCheckQueries

---
+++ freqline-be/src/main/java/ch/giuliobosco/freqline/queries/AccCheckQueries.java
@@ -0,0 +1,115 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package ch.giuliobosco.freqline.queries;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Acc Check queries.
+ *
+ * @author giuliobosco (giuliobva@gmail.com)
+ * @version 1.0 (2019-11-15 - 2019-11-15)
+ */
+public class AccCheckQueries {
+    // ------------------------------------------------------------------------------------ Costants
+
+    /**
+     * Check generator key query.
+     */
+    private static final String GENERATOR_KEY_CHECK = "SELECT count(id) FROM generator WHERE key_c=?";
+
+    /**
+     * Check mic key query.
+     */
+    private static final String MIC_KEY_CHECK = "SELECT count(id) FROM mic WHERE key_c=?";
+
+    /**
+     * Check remote key query.
+     */
+    private static final String REMOTE_KEY_CHECK = "SELECT count(id) FROM remote WHERE key_c=?";
+
+    // --------------------------------------------------------------------------- Static Components
+
+    /**
+     * Check if key is valid.
+     *
+     * @param query      Query.
+     * @param connection Connection to MySQL database.
+     * @param keyC       Key to check.
+     * @return True if key is valid.
+     * @throws SQLException Error with MySQL.
+     */
+    private static boolean isValidKey(String query, Connection connection, String keyC) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, keyC);
+
+        ResultSet resultSet = statement.executeQuery();
+
+        if (!resultSet.next()) {
+            return false;
+        }
+
+        return resultSet.getInt(1) > 0;
+    }
+
+    /**
+     * Check if generator key is valid.
+     *
+     * @param connection Connection to MySQL database.
+     * @param keyC       Key to check.
+     * @return True if key is valid.
+     * @throws SQLException Error with MySQL.
+     */
+    public static boolean isValidGeneratorKey(Connection connection, String keyC) throws SQLException {
+        return isValidKey(GENERATOR_KEY_CHECK, connection, keyC);
+    }
+
+    /**
+     * Check if mic key is valid.
+     *
+     * @param connection Connection to MySQL database.
+     * @param keyC       Key to check.
+     * @return True if key is valid.
+     * @throws SQLException Error with MySQL.
+     */
+    public static boolean isValidMicKey(Connection connection, String keyC) throws SQLException {
+        return isValidKey(MIC_KEY_CHECK, connection, keyC);
+    }
+
+    /**
+     * Check if remote key is valid.
+     *
+     * @param connection Connection to MySQL database.
+     * @param keyC       Key to check.
+     * @return True if key is valid.
+     * @throws SQLException Error with MySQL.
+     */
+    public static boolean isValidRemoteKey(Connection connection, String keyC) throws SQLException {
+        return isValidKey(REMOTE_KEY_CHECK, connection, keyC);
+    }
+}
