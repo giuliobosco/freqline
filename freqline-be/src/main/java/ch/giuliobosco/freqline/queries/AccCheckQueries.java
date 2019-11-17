1614310b321f05dbf39f832b4d6dbd86ccadb04a
giuliobosco <giuliobva@gmail.com>
2019-11-17 20:10:20 +0100

    add queries.AccCheckQueries - getJsonConf()

--- freqline-be/src/main/java/ch/giuliobosco/freqline/queries/AccCheckQueries.java
+++ freqline-be/src/main/java/ch/giuliobosco/freqline/queries/AccCheckQueries.java
@@ -24,6 +24,8 @@
 
 package ch.giuliobosco.freqline.queries;
 
+import org.json.JSONObject;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -33,7 +35,7 @@ import java.sql.SQLException;
  * Acc Check queries.
  *
  * @author giuliobosco (giuliobva@gmail.com)
- * @version 1.0 (2019-11-15 - 2019-11-15)
+ * @version 1.0.1 (2019-11-15 - 2019-11-17)
  */
 public class AccCheckQueries {
     // ------------------------------------------------------------------------------------ Costants
@@ -53,7 +55,42 @@ public class AccCheckQueries {
      */
     private static final String REMOTE_KEY_CHECK = "SELECT count(id) FROM remote WHERE key_c=?";
 
+    /**
+     * Query for acc auto conf.
+     */
+    private static final String CONF_QUERY = "SELECT g.name AS gname, g.frequence as gfreqence, g.key_c AS gkey, g.ip AS gip, m.timer AS mtimer, m.key_c AS mkey, r.key_c AS rkey FROM generator g JOIN remote r ON g.id = r.generator JOIN mic m ON g.id = m.generator WHERE g.key_c = ?";
+
     // --------------------------------------------------------------------------- Static Components
+
+    /**
+     * Get the json configuration for acc.
+     *
+     * @param connection Connection to MySQL database.
+     * @param keyC       Key of communication of the generator.
+     * @return Json configuration of the generator.
+     * @throws SQLException Error with MySQL.
+     */
+    public static JSONObject getJsonConf(Connection connection, String keyC) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(CONF_QUERY);
+        statement.setString(1, keyC);
+        ResultSet resultSet = statement.executeQuery();
+
+        if (!resultSet.next()) {
+            return null;
+        }
+
+        JSONObject jo = new JSONObject();
+
+        jo.put("gname", resultSet.getString("gname"));
+        jo.put("gfrequence", resultSet.getString("gfrequence"));
+        jo.put("gkey", resultSet.getString("gkey"));
+        jo.put("gip", resultSet.getString("gip"));
+        jo.put("mtimer", resultSet.getString("mtimer"));
+        jo.put("mkey", resultSet.getString("rkey"));
+        jo.put("rkey", resultSet.getString("rkey"));
+
+        return jo;
+    }
 
     /**
      * Check if key is valid.
