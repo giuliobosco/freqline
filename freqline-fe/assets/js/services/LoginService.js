854e442a9801ad5244ed2bba6ee98de473c17b61
giuliobosco <giuliobva@gmail.com>
2019-11-21 15:37:02 +0100

    fix fe/LoginService - Access-Control-Allow-Origin

--- freqline-fe/assets/js/services/LoginService.js
+++ freqline-fe/assets/js/services/LoginService.js
@@ -1,16 +1,15 @@
 app.factory('LoginService', ['$http', function($http) {
     var service = {};
 	var address = "localhost";
-	var port = 8080;
-    var urlBase = "http://" + address + ":" + port;
+    var port = 8080;
+    var baseApi = "/freqline-be"
+    var urlBase = "http://" + address + ":" + port + baseApi;
 	urlBase += "/action/login";
 
     service.login = function (username, password) { 
-		let url = urlBase + "?username=" + username + "&password=" + password;
-        return $http({
-            method: 'POST',
-            url: url
-        }).then(function (response){
+        let url = urlBase + "?t=" + new Date().getTime() + "&username=" + username + "&password=" + password;
+		let data = "username=" + username + "&password=" + password;
+        return $http.post(url, data, {headers: {'Access-Control-Allow-Origin': ' *' } }).then(function (response){
             return response.data;
         },function (error){
             return error;
