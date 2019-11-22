4609c18321f92b50307989e0c1fecda1269c9715
giuliobosco <giuliobva@gmail.com>
2019-11-22 13:49:47 +0100

    fix servlets.BaseServlet - ok() return content-type

--- freqline-be/src/main/java/ch/giuliobosco/freqline/servlets/BaseServlet.java
+++ freqline-be/src/main/java/ch/giuliobosco/freqline/servlets/BaseServlet.java
@@ -424,10 +424,11 @@ public abstract class BaseServlet extends HttpServlet {
      */
     private void writeStatus(HttpServletResponse response, int statusCode, String statusString, JSONObject jo) throws IOException {
         response.setStatus(statusCode);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Content-Type", "application/json");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         response.setHeader("Access-Control-Max-Age", "86400");
         jo.put(S_STATUS_CODE, statusString);
         write(response, jo.toString());
@@ -555,9 +556,10 @@ public abstract class BaseServlet extends HttpServlet {
     protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
         String origin = request.getHeader("origin");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Origin",origin);
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         response.setHeader("Access-Control-Max-Age", "86400");
     }
 }
