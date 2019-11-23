4b75245a9271bf47029ecd6a95aa1eed3763e627
giuliobosco <giuliobva@gmail.com>
2019-11-23 21:14:40 +0100

    add fe/LoginService - isLoggedIn(), getPermissions()

--- freqline-be/src/main/webapp/assets/js/services/LoginService.js
+++ freqline-be/src/main/webapp/assets/js/services/LoginService.js
@@ -1,4 +1,5 @@
 app.factory('LoginService', ['$http', function($http) {
+    var config = {headers: [{'Access-Control-Allow-Origin': ' *' },{'Access-Control-Allow-Credentials':'true'}]};
     var service = {};
 	var address = "localhost";
     var port = 8080;
@@ -10,12 +11,34 @@ app.factory('LoginService', ['$http', function($http) {
     service.login = function (username, password) { 
         let url = urlBase + "?t=" + new Date().getTime() + "&username=" + username + "&password=" + password;
 		let data = "username=" + username + "&password=" + password;
-        return $http.post(url, data, {headers: [{'Access-Control-Allow-Origin': ' *' },{'Access-Control-Allow-Credentials':'true'}]}).then(function (response){
+        return $http.post(url, data, ).then(function (response){
             return response.data;
         },function (error){
             return error;
         });
     };
+
+    service.isLoggedIn = function() {
+        let url = urlBase + "?t=" + new Date().getTime();
+        let data = "";
+
+        return $http.get(url, data, config).then(function (response) {
+            return {response: response.data.isLoggedIn};
+        }, function(error) {
+            return {response: false};
+        });
+    }
+
+    service.getPermissions = function() {
+        let url = urlBase + "/permissions?t=" + new Date().getTime();
+        let data = "";
+
+        return $http.get(url, data, config).then(function (response) {
+            return response.data.permissions;
+        }, function(error) {
+            return error;
+        });
+    }
 
     return service;
 }]);
