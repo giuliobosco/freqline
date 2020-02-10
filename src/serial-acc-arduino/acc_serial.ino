1c76b39bddec76555990a5e83103d39ef62a9063
giuliobosco <giuliobva@gmail.com>
2020-02-10 15:14:43 +0100

    fix serial-acc-arduino/acc_serial - response

--- src/serial-acc-arduino/acc_serial.ino
+++ src/serial-acc-arduino/acc_serial.ino
@@ -2,6 +2,27 @@
 #define SERIAL_INIT 42                        // serial initialized char
 #define SERIAL_END_OF_COMMAND '\n'            // End of the command char
 
+char instruction;                             // instruction variable
+String instructionValue;                      // instruction value variable
+
+/**
+ * Get the instruction.
+ * 
+ * @return Instruction.
+ */
+char getInstruction() {
+  return instruction;
+}
+
+/**
+ * Get the instruction value.
+ * 
+ * @return Instruction value.
+ */
+String getInstructionValue() {
+  return instructionValue;
+}
+
 /**
  * Initialization of serial communication.
  */
@@ -13,36 +34,82 @@ void serialSetup() {
 }
 
 /**
- * Read command from serial.
+ * Read instruction from serial.
  */
-void readCommand() {
-  // read command from serial line
+boolean serialRead() {
+  // read from serial line
   String s = Serial.readStringUntil(SERIAL_END_OF_COMMAND);
 
-  // if command exists
+  // if correctly read exists
   if (s.length() > 0) {
-    // set command char to command variable
-    command = s[0];
-    // set command value to command value variable
-    commandValue = s.substring(1);
+    // set command char to instruction variable
+    instruction = s[0];
+    // set command value to instruction value variable
+    instructionValue = s.substring(1);
+    return true;
   }
+
+  return false;
 }
 
 /**
- * Write command to serial.
- * 
- * @param command command char to write to serial
- * @param value value of the command to wirte to serial
+ * Read command from serial.
  */
-void writeCommand(char command, String value) {
-  // write command to serial
-  Serial.write(command);
+void serialReadCommand() {
+  serialRead();
+}
+
+/**
+ * Read response from serial.
+ * 
+ * @return True if response is valid.
+ */
+boolean serialReadResponse() {
+  return serialRead();
+}
+
+/**
+ * Write to serial.
+ * 
+ * @param instruction Instruction.
+ * @param value Instruction value.
+ */
+void serialWrite(char instruction, String value) {
+  // write instruction to serial
+  Serial.write(instruction);
 
   // write all chars of the value to serial
   for (int i = 0; i < value.length(); i++) {
     Serial.write(value[i]);
   }
 
-  // write end of command char to serial
+  // write end of message char to serial
   Serial.write(SERIAL_END_OF_COMMAND);
+}
+
+/**
+ * Write response to serial.
+ * 
+ * @param response Response char to write to serial.
+ * @param value value of the response to write to serial.
+ */
+void serialWriteResponse(char response, String value) {
+  serialWrite(response, value);
+}
+
+/**
+ * Write command to serial.
+ * 
+ * @param command Command char to write to serial.
+ * @param value Value of the command to write to write.
+ */
+void serialWriteCommand(char command, String value) {
+  boolean commandSent = false;
+
+  while (!commandSent) {
+    serialWrite(command, value);
+
+    commandSent = serialReadResponse();
+  }
+  
 }
